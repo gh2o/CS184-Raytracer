fc9300c279a1eca3ff3a8312c4b4a5d9d4120976
Linda He <linda@JimHeWork.local>
2014-10-08 00:33:33 -0700

    readding files

--- as2.cpp
+++ as2.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <Eigen/Dense>
 
+using namespace Eigen;
+
 int main(int argc, char *argv[]) {
+
+typedef Matrix<double, 3, 1> Color;
+
+class Ray () {
+public:
+	Vector4d origin_;
+	Vector4d direction_;
+	float t_;
+}
+
+class BRDF() {
+	double specularCoeffecient;
+	float ambientColor;
+	Color ambientColor;
+
+}
+
+
+
 	return 0;
 }
