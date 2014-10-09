678a1459decf4807b6fca177227b50ca188e4430
gh2o <git@thegavinli.com>
2014-10-09 02:46:09 -0700

    add and use util functions for non-homogeneous conversions

--- as2.cpp
+++ as2.cpp
@@ -52,10 +52,20 @@ public:
 
 class Util {
 public:
-	static Vector4d cross(Vector4d a, Vector4d b) {
+	static Vector4d cross(const Vector4d& a, const Vector4d& b) {
 		Vector4d c = Vector4d::Zero();
 		c.head<3>() = a.head<3>().cross(b.head<3>());
 		return c;
+	}
+	/* NOTE: vec?dFrom?d functions should only be used with
+	 *       directional (non-homogeneous) vectors. */
+	static Vector4d vec4dFrom3d(const Vector3d& f) {
+		Vector4d t = Vector4d::Zero();
+		t.head<3>() = f;
+		return t;
+	}
+	static Vector3d vec3dFrom4d(const Vector4d& f) {
+		return f.head<3>();
 	}
 };
 
@@ -389,10 +399,10 @@ public:
 				return Vector3d(&params[offset]).homogeneous();
 			};
 			auto dvec = [&](int offset) {
-				Vector4d r(0,0,0,0);
-				r.head<3>() = Vector3d(&params[offset]);
-				r.normalize();
-				return r;
+				Vector3d c(&params[offset]);
+				if (c.isZero())
+					throw ParseException("zero direction specified", lineno);
+				return Util::vec4dFrom3d(c.normalized());
 			};
 			Vector3d fvec = cvec(0);
 			switch (ltype.type_) {
