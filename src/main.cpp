214e85287681ac7350a2f792337610be395770c9
Gavin Li <gavinli@thegavinli.com>
2014-10-10 08:18:57 -0700

    add progress indicator to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
+#include <signal.h>
+#include <sys/time.h>
 #include <iostream>
 #include <fstream>
 #include "options.h"
 #include "scene.h"
 #include "parsers.h"
 #include "writers.h"
+
+static volatile bool progressSignaled = false;
+
+static void updateProgress(int complete, int total) {
+	if (complete != total && !progressSignaled)
+		return;
+	std::putchar('\r');
+	std::printf("Rendering scene (%d/%d) (%.1f%%) ...",
+			complete, total, 100.0 * complete / total);
+	std::fflush(stdout);
+	if (complete == total)
+		std::putchar('\n');
+	progressSignaled = false;
+}
+
+static void alarmProgress(int sig) {
+	progressSignaled = true;
+}
+
+static void setupAlarm(bool enable) {
+	if (enable)
+		signal(SIGALRM, alarmProgress);
+	suseconds_t period = enable ? 1000000 / 4 : 0;
+	struct itimerval itv = {{0}};
+	itv.it_value.tv_usec = period;
+	itv.it_interval.tv_usec = period;
+	setitimer(ITIMER_REAL, &itv, nullptr);
+	if (!enable)
+		signal(SIGALRM, SIG_DFL);
+}
 
 int main(int argc, char *argv[]) {
 	// program options
@@ -33,9 +65,13 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	// render scene
+	signal(SIGALRM, alarmProgress);
+	setupAlarm(true);
 	Scene::RasterImage image(programOptions.renderHeight_,
 	                               programOptions.renderWidth_);
-	scene.renderScene(image);
+	scene.renderScene(image, updateProgress);
+	setupAlarm(false);
+	signal(SIGALRM, SIG_DFL);
 	// write output
 	try {
 		PNGWriter writer(outputFilename);
