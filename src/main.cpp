bfa944a399bd219b21327d08deef68f8026d567a
gh2o <git@thegavinli.com>
2014-10-09 23:37:41 -0700

    split out rtbase

--- src/main.cpp
+++ src/main.cpp
@@ -13,54 +13,7 @@
 #include "exceptions.h"
 #include "options.h"
 #include "util.h"
-
-class Ray {
-public:
-	Ray(Vector4d ori, Vector4d dir) {
-		origin(ori);
-		direction(dir);
-	}
-	Vector4d origin() { return origin_; }
-	Vector4d direction() { return direction_; }
-	void origin(const Vector4d& ori) {
-		if (ori(3) == 0)
-			throw MathException("ray origin is a direction vector");
-		origin_ = ori;
-	}
-	void direction(const Vector4d& dir) {
-		if (dir.isZero())
-			throw MathException("ray has no direction");
-		if (dir(3) != 0)
-			throw MathException("ray direction is a point vector");
-		direction_ = dir.normalized();
-	}
-private:
-	Vector4d origin_;
-	Vector4d direction_;
-};
-
-class Material {
-public:
-	Color3d ambientColor_;
-	Color3d diffuseColor_;
-	Color3d specularColor_;
-	Color3d reflectiveColor_;
-	double specularCoefficient_;
-};
-
-class Transformable {
-public:
-	Transform4d transform_;
-};
-
-class Camera : public Transformable {
-public:
-	Vector4d eyePoint_;
-	Vector4d lowerLeftPoint_;
-	Vector4d lowerRightPoint_;
-	Vector4d upperLeftPoint_;
-	Vector4d upperRightPoint_;
-};
+#include "rtbase.h"
 
 class Light : public Transformable {
 public:
