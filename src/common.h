157b703c0ab659e7abfb28af27de1ea6619f9e88
gh2o <git@thegavinli.com>
2014-10-09 23:01:50 -0700

    split rename

---
+++ src/common.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+using namespace Eigen;
+
+typedef Transform<double,3,Affine> Transform4d;
+typedef Array<double,3,1> Color3d;
--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,7 @@
 #include <vector>
 #include <array>
 #include <limits>
-#include <Eigen/Core>
-#include <Eigen/Geometry>
-
-using namespace Eigen;
-
-typedef Transform<double,3,Affine> Transform4d;
-typedef Array<double,3,1> Color3d;
+#include "common.h"
 
 class ParseException : public std::runtime_error {
 public:
