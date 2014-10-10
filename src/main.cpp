24fdabbbf19889f09bd02b48f6349836514926d8
gh2o <git@thegavinli.com>
2014-10-09 23:29:29 -0700

    split out util

--- src/main.cpp
+++ src/main.cpp
@@ -12,25 +12,7 @@
 #include "common.h"
 #include "exceptions.h"
 #include "options.h"
-
-class Util {
-public:
-	static Vector4d cross(const Vector4d& a, const Vector4d& b) {
-		Vector4d c = Vector4d::Zero();
-		c.head<3>() = a.head<3>().cross(b.head<3>());
-		return c;
-	}
-	/* NOTE: vec?dFrom?d functions should only be used with
-	 *       directional (non-homogeneous) vectors. */
-	static Vector4d vec4dFrom3d(const Vector3d& f) {
-		Vector4d t = Vector4d::Zero();
-		t.head<3>() = f;
-		return t;
-	}
-	static Vector3d vec3dFrom4d(const Vector4d& f) {
-		return f.head<3>();
-	}
-};
+#include "util.h"
 
 class Ray {
 public:
